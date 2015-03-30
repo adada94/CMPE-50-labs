b5b8bef39a3a647cef62df291ae609a08cf68a59
adisha94 <abetawar@gmail.com>
2015-03-30 15:00:10 -0700

    uploaded new files for execercise 2 for lab 6
    Lab 6 execercise 2

---
+++ Lab#6exe2.cpp
@@ -0,0 +1,14 @@
+////
+////  Lab#6exe2.cpp
+////  C++_Central
+////
+////  Created by Adish Betawar on 3/30/15.
+////  Copyright (c) 2015 Adish Betawar. All rights reserved.
+////
+//
+//#include "EaseOfAccesss.h"
+//
+//int main()
+//{
+//    return 1;
+//}
