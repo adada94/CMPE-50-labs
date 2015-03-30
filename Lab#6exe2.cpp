d1387bd81d39461c0e1c36fb4942778cedda08b4
adisha94 <abetawar@gmail.com>
2015-03-30 15:41:03 -0700

    started execercise 2
    this is for Excercise 2 of lab#6

--- Lab#6exe2.cpp
+++ Lab#6exe2.cpp
@@ -1,14 +1,14 @@
-////
-////  Lab#6exe2.cpp
-////  C++_Central
-////
-////  Created by Adish Betawar on 3/30/15.
-////  Copyright (c) 2015 Adish Betawar. All rights reserved.
-////
 //
-//#include "EaseOfAccesss.h"
+//  Lab#6exe2.cpp
+//  C++_Central
 //
-//int main()
-//{
-//    return 1;
-//}
+//  Created by Adish Betawar on 3/30/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#include "EaseOfAccesss.h"
+
+class Rational
+{
+    
+}rational;
