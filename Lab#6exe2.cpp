ff8927f1f02c6bf7763542e9ce127b8ad2351348
adisha94 <abetawar@gmail.com>
2015-03-30 21:38:21 -0700

    major update

--- Lab#6exe2.cpp
+++ Lab#6exe2.cpp
@@ -9,6 +9,79 @@
 #include "EaseOfAccesss.h"
 #include <fstream>
 /*
- * Exercise 1
+ * Exercise 2
  *
  */
+
+// class used for rational numbers
+class Rational
+{
+
+private:
+
+    int numerator;
+    int denominator;
+
+public:
+    
+    // default constructor
+    Rational();
+    
+    // accessors
+    int getNumerator();
+    int getDenominator();
+    
+    // mutators
+    void setNumerator();
+    void setDenominator();
+
+    // member functions]
+    void Input(istream& in);
+    void Output(ostream& out);
+    // friend functions
+    friend Rational Add();
+    friend Rational Substract();
+    friend Rational Multiply();
+    friend Rational Division();
+    
+};
+
+
+
+
+
+int main()
+{
+    
+}
+
+
+
+// adds the fractions
+Rational Add()
+{
+    
+}
+
+
+//substracts the fractions
+Rational Substract()
+{
+    
+}
+
+// multiplies the fractions
+Rational Multiply()
+{
+    
+}
+
+
+
+// divides the fraction
+Rational Division()
+{
+    
+}
+
+
