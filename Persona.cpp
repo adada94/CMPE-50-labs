c66d3d5c1b1d5a8c833715036dbdfe9621492201
adisha94 <abetawar@gmail.com>
2015-03-30 20:23:13 -0700

    clearing console
    for those who like to keep the screen on the console clean.

---
+++ Persona.cpp
@@ -0,0 +1,9 @@
+//
+//  Persona.cpp
+//  C++_Central
+//
+//  Created by Adish Betawar on 3/30/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#include "Persona.h"
---
+++ Persona.h
@@ -0,0 +1,14 @@
+//
+//  Persona.h
+//  C++_Central
+//
+//  Created by Adish Betawar on 3/30/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#ifndef __C___Central__Persona__
+#define __C___Central__Persona__
+
+#include <stdio.h>
+
+#endif /* defined(__C___Central__Persona__) */
