12a3ce09c450348d1f8b7a2e26c02f53fbb0aeda
adisha94 <abetawar@gmail.com>
2015-04-06 15:06:08 -0700

    update 6
    the last update was update 5 sorry. working on the file IO portion of
    the assignment

--- Lab#7.cpp
+++ Lab#7.cpp
@@ -58,6 +58,26 @@ int main()
     // begin asking for seat numbers and assign them accordingly
     
     
+    int option;
     cout << "\n\nWould you like to enter the information from the keyboard or from another file?\n";
     cout << "\t\t\t1. keyboard \t\t\t2. file";
+    cin >> option;
+    
+    if (option == 1)
+    {
+        cout << "You have chosen to provide info with keyboard";
+    }
+    else if (option == 2)
+    {
+        chdir("//users//adish//desktop");
+        string filename;
+        cout << "You have chosen to provide info with a file. What is name of the file that contains the seating arrangement?" << endl;
+        cin >> filename;
+        input.open(filename);
+        if (input.fail())
+        {
+            cout << "File does not exist";
+        }
+        
+    }
 }
