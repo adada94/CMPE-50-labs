8b8d9a87ae53797ad2f74151860c25bce712e517
adisha94 <abetawar@gmail.com>
2015-04-06 14:27:27 -0700

    update 3
    trying a different approach if not mentioned in the previous update.
    Just trying to generalize the really long if statement for the numbers.

--- Lab#7.cpp
+++ Lab#7.cpp
@@ -11,6 +11,8 @@
 
 int main()
 {
+    ifstream input;
+    input.open("//users//adish//desktop//input.txt");
     // defining dynamic array
     int rowSize;
 //    char *colA;
@@ -18,12 +20,13 @@ int main()
 //    char *colC;
 //    char *colD;
 
-    
+
+//    cout << "How many passengers are in a row on this airline?" << endl;
+    input >> rowSize;
+
     // getting input for number of seats in an airplane
     char **seats;
-    
-    cout << "How many passengers are in a row on this airline?" << endl;
-    cin >> rowSize;
+
     seats = new char*[rowSize];
     
 //    colA = new char[rowSize];
@@ -31,10 +34,62 @@ int main()
 //    colC = new char[rowSize];
 //    colD = new char[rowSize];
 
+    // acquiring info from file to put into array for seats
     for (int i = 0; i < rowSize; i++)
     {
-        seats = new char*[4];
+        seats[i] = new char[4];
+    }
+    input.close();
+    
+
+    //outputting the seating arrangement
+    for (int i = 0; i < rowSize; i++) // rows seating
+    {
+        for (int j = 0; j < rowSize; j++) // columns seating
+        {
+            if (seats[i][0] == '1')
+            {
+                cout << seats[i][j];
+                cout << "\n";
+            }
+            else if (seats[i][0] == '2')
+            {
+                cout << seats[i][j];
+                cout << "\n";
+            }
+            else if (seats[i][0] == '3')
+            {
+                cout << seats[i][j];
+                cout << "\n";
+            }
+            else if (seats[i][0] == '4')
+            {
+                cout << seats[i][j];
+                cout << "\n";
+            }
+            
+            else if (seats[i][0] == '5')
+            {
+                cout << seats[i][j];
+                cout << "\n";
+            }
+            else if (seats[i][0] == '6')
+            {
+                cout << seats[i][j];
+                cout << "\n";                
+            }
+            else
+            {
+                seats[i][j] = 65 + j; // converting the ASCII character to get characters 'A' 'B'.... etc.
+            }
+            cout << seats[i][j] << " "; // outputting the seats in this order
+
+        }
+        cout << "\n";
     }
     
-    
 }
+
+
+
+
