cd7bc645a3520bb82775757f86a67116c1110a7c
adisha94 <abetawar@gmail.com>
2015-04-06 13:37:59 -0700

    beggining lab#7
    intial commit for lab#7

---
+++ Lab#7.cpp
@@ -0,0 +1,18 @@
+//
+//  Lab#7.cpp
+//  C++_Central
+//
+//  Created by Adish Betawar on 4/6/15.
+//  Copyright (c) 2015 Adish Betawar. All rights reserved.
+//
+
+#include <stdio.h>
+#include "EaseOfAccesss.h"
+
+int main()
+{
+    char *colA;
+    char *colB;
+    char *colC;
+    char *colD;
+}
