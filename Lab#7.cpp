057c64213cc0f563b8d6b8eb53a65e8917d9b269
adisha94 <abetawar@gmail.com>
2015-04-11 14:39:24 -0700

    update 13

--- Lab#7.cpp
+++ Lab#7.cpp
@@ -9,6 +9,14 @@
 #include <stdio.h>
 #include "Persona.h"
 #include "Helper.h"
+
+
+
+typedef char* Char_ptr;
+
+// function declarations
+void PrintSeatingArrangement(Char_ptr *seats, int rowSize, int numberOfSeatsPerRow);
+
 int main()
 {
     cout << "Excercise 1\n";
@@ -17,65 +25,63 @@ int main()
     
     ifstream input;
     input.open("//users//adish//desktop//input.txt");
-    // defining dynamic array
+    //
     int rowSize;
     cout << "How many rows are in the plane?\n"; // defining number of rows
     input >> rowSize;
+    const int seatsInEachRow = 4;
+    cout << "There are " << rowSize << " rows and " << seatsInEachRow << " seats in each row on this flight\n";
+    //
     
     // getting input for number of seats in an airplane using dynamic array
-    typedef char* Char_ptr;
-    Char_ptr *seats; // number of seats
-    seats = new Char_ptr[rowSize]; // dynamic number of seats
-    
-    // acquiring info from file to put into array for seats
+    Char_ptr *seats = new Char_ptr[rowSize]; // dynamcally allocating memory for rows
+    //
+    // allocating memory for each row
     for (int i = 0; i < rowSize; i++)
     {
-        seats[i] = new char[4]; // ??
+        seats[i] = new char[seatsInEachRow]; // number of seats for each row
     }
-    input.close();
-    
+    //
     cout << "Today's seating arrangement" << endl;
     printLine(25, '-');
-    //outputting the seating arrangement
+    //
+    // filling up the initial seating arrangemnts
     for (int i = 0; i < rowSize; i++) // rows seating
     {
         for (int j = 0; j < 4; j++) // columns seating
         {
             seats[i][j] = 65 + j; // converting the ASCII character to get characters 'A' 'B'.... etc.
-            cout << seats[i][j] << " "; // outputting the seats in this order
-            
         }
-        cout << "\n";
     }
-    
+    PrintSeatingArrangement(seats, rowSize, seatsInEachRow);
     // begin asking for seat numbers and assign them accordingly
     
 
-//    int option;
-//    cout << "\n\nWould you like to enter the information from the keyboard or from another file?\n";
-//    cout << "\t\t\t1. keyboard \t\t\t2. file\n";
-//    cin >> option;
-//    int seats_Available;
-//    if (option == 1) // option for getting user from keyboard
-//    {
-//        int row = 0;
-//        char column = 'A';
-//        cout << "You have chosen to provide info with keyboard" << endl;
-//        cout << "What row number would you like on this airline?\n Choose between 1 <--> 7" << endl;
-//        cout << "Seats available: " << seats_Available << endl;
-//        cin >> row;
-//        if (row > 7)
-//        {
-//            cout << "This row does not exist";
-//        }
-//        cout << "What row number would you like on this airline?\n Choose between A <--> D" << endl;
-//        cin >> column;
-//        if (column != 'A')
-//        {
-//            cout << "This column does not exist" << endl;
-//        }
-//        
-//    }
+    int option;
+    cout << "\n\nWould you like to enter the information from the keyboard or from another file?\n";
+    cout << "\t\t\t1. keyboard \t\t\t2. file\n";
+    cin >> option;
+    int seats_Available;
+    if (option == 1) // option for getting user from keyboard
+    {
+        int row = 0;
+        char column = 'A';
+        cout << "You have chosen to provide info with keyboard" << endl;
+        cout << "What row number would you like on this airline?\n Choose between 1 <--> 7" << endl;
+        cout << "Seats available: " << seats_Available << endl;
+        cin >> row;
+        if (row > 7)
+        {
+            cout << "This row does not exist";
+        }
+        cout << "What row number would you like on this airline?\n Choose between A <--> D" << endl;
+        cin >> column;
+        if (column != 'A')
+        {
+            cout << "This column does not exist" << endl;
+        }
+        
+    }
 //    else if (option == 2) // file IO. getting input from file
 //    {
 //        chdir("//users//adish//desktop"); // we change the default directory to the desktop
@@ -89,6 +95,26 @@ int main()
 //        }
 //        input.close();
 //    }
-    delete seats;
+    for (int i = 0; i < rowSize; i++)
+    {
+        delete seats[i]; // deleting memory for each row
+    }
 
+    delete[] seats; // deleting pointers to all rows
+
+}
+
+
+
+
+void PrintSeatingArrangement(Char_ptr *seats, int rowSize, int numberOfSeatsPerRow)
+{
+    for (int i = 0; i < rowSize; i++) // rows seating
+    {
+        for (int j = 0; j < numberOfSeatsPerRow; j++) // columns seating
+        {
+            cout << seats[i][j] << " "; // outputting the seats in this order
+        }
+        cout << "\n";
+    }
 }
